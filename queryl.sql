9dc3a919b93599e85725888c54df50466b86647b
emeka egenaonu <emekaegenaonu@gmail.com>
2026-06-19 14:43:55 +0100

    Added first SQL query - employee selection

---
+++ queryl.sql
@@ -0,0 +1,18 @@
+SQL QUERIES
+Author: Egenaonu Emeka
+Date: 19-06-2026
+
+SELECT
+first_name,
+last_name,
+email,
+hire_date
+
+FROM
+employees
+
+WHERE
+department = 'Sales'
+
+ORDER BY
+hire_date DESC;
